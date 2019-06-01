ad4cdcd94441e87684222d12b78776659504c5b2
keyhan66 <hesam.emami9@gmail.com>
2019-06-01 15:10:39 +0430

    Refactored Recycler Adapter-1

---
+++ app/src/main/java/com/emami/android/comicworld/util/BindingAdapter.kt
@@ -0,0 +1,28 @@
+package com.emami.android.comicworld.util
+
+import android.graphics.Outline
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
+import ss.com.bannerslider.Slider
+
+
+@BindingAdapter("app:clip_slider")
+fun Slider.clipImageViewCorners(enabled: Boolean) {
+    if (enabled) {
+        this.outlineProvider = object : ViewOutlineProvider() {
+
+            override fun getOutline(view: View?, outline: Outline?) {
+                outline?.setRoundRect(0, 0, (view!!.width + 90f).toInt(), view.height, 100f)
+            }
+        }
+        this.clipToOutline = true
+    }
+}
+
+@BindingAdapter("bannerSrc")
+fun ImageView.loadComicBanner(imgSrc: String) {
+    Picasso.get().load(imgSrc).into(this)
+}
