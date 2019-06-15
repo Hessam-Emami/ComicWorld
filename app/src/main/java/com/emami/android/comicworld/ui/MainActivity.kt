5d0c1e832a9253ec44ce1beab7cbfc1d48cb9894
keyhan66 <hesam.emami9@gmail.com>
2019-06-15 13:36:54 +0430

    added connectivity cheker!

--- app/src/main/java/com/emami/android/comicworld/ui/MainActivity.kt
+++ app/src/main/java/com/emami/android/comicworld/ui/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.emami.android.comicworld.R
-import com.emami.android.comicworld.data.Test
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.android.support.DaggerAppCompatActivity
-import timber.log.Timber
-import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
 
