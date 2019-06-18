321e91c2e45327aaa5b66b30388ceadd79ee94ba
keyhan66 <hesam.emami9@gmail.com>
2019-06-18 17:31:00 +0430

    SECC

--- app/src/main/java/com/emami/android/comicworld/data/Repository.kt
+++ app/src/main/java/com/emami/android/comicworld/data/Repository.kt
@@ -4,7 +4,9 @@ import com.emami.android.comicworld.data.network.ComicDataProvider
 import com.emami.android.comicworld.data.network.ComicDataState
 import com.emami.android.comicworld.data.network.DataProvider
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class Repository @Inject constructor(
 
     val dataProvider: ComicDataProvider
--- app/src/main/java/com/emami/android/comicworld/di/module/AppModule.kt
+++ app/src/main/java/com/emami/android/comicworld/di/module/AppModule.kt
@@ -7,7 +7,6 @@ import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-//TODO SIMPLIFY DEPENDENCIES!!!!AND INJECTIONS!!!!
 @Module
 object AppModule {
 
